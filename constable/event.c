Medusa-Team/Constable

commit a06e00b1681510047cfd37d44028c7320a3fc023

refactor: meaningful names

constable/event.c
@@ -101,7 +101,7 @@ int event_free_all_events( struct comm_s *comm )
 
 struct event_type_s *event_type_add( struct comm_s *comm, struct medusa_acctype_s *m, struct medusa_attribute_s *a )
 { struct event_type_s *e;
-    struct event_names_s *p;
+    struct event_names_s *evname;
     int l;
     //printf("ZZZ event_from_medusa:\n");
     for(l=0;a[l].type!=MED_TYPE_END;l++);
@@ -109,7 +109,7 @@ struct event_type_s *event_type_add( struct comm_s *comm, struct medusa_acctype_
     l*=sizeof(struct medusa_attribute_s);
     if( (e=malloc(sizeof(struct event_type_s)+l))==NULL )
         return(NULL);
-    memcpy(&(e->m),m,sizeof(struct medusa_acctype_s));
+    memcpy(&(e->acctype),m,sizeof(struct medusa_acctype_s));
     memset(&(e->operation_class),0,sizeof(e->operation_class));
     //	e->operation_class.next=NULL;
     //	e->operation_class.cinfo_offset=e->operation_class.cinfo_size=0;
@@ -117,30 +117,30 @@ struct event_type_s *event_type_add( struct comm_s *comm, struct medusa_acctype_
     //	e->operation_class.set=NULL;
     //	e->operation_class.event_offset=e->operation_class.event_size=0;
     //	e->operation_class.m.classid=0;
-    e->operation_class.m.size=e->m.size;
+    e->operation_class.m.size=e->acctype.size;
     e->operation_class.m.name[0]=0;
-    strncpy(e->operation_class.m.name,e->m.name,MIN(MEDUSA_CLASSNAME_MAX,MEDUSA_OPNAME_MAX));
+    strncpy(e->operation_class.m.name,e->acctype.name,MIN(MEDUSA_CLASSNAME_MAX,MEDUSA_OPNAME_MAX));
     memcpy(e->operation_class.attr,a,l);
     e->operation_class.comm=comm;
 
-    if( (e->op[0]=(struct class_s*)hash_find(&(comm->classes),e->m.op_class[0]))==NULL )
-        init_error("Subject of access %s does not exist!",e->m.name);
-    if( e->m.op_class[0]==e->m.op_class[1] && !strncmp(e->m.op_name[0],e->m.op_name[1],MEDUSA_ATTRNAME_MAX) )
+    if( (e->op[0]=(struct class_s*)hash_find(&(comm->classes),e->acctype.op_class[0]))==NULL )
+        init_error("Subject of access %s does not exist!",e->acctype.name);
+    if( e->acctype.op_class[0]==e->acctype.op_class[1] && !strncmp(e->acctype.op_name[0],e->acctype.op_name[1],MEDUSA_ATTRNAME_MAX) )
         e->op[1]=NULL;
-    else	e->op[1]=(struct class_s*)hash_find(&(comm->classes),e->m.op_class[1]);
+    else	e->op[1]=(struct class_s*)hash_find(&(comm->classes),e->acctype.op_class[1]);
     event_mask_clear2(&(e->mask[0]));
     // if event is TRIGERED_AT_OBJECT
-    if( e->m.actbit&0x8000 )
-        e->object=e->op[1];	/* object */
+    if( e->acctype.actbit&0x8000 )
+        e->monitored_operand=e->op[1];	/* object */
     else
-        e->object=e->op[0];	/* subject */
+        e->monitored_operand=e->op[0];	/* subject */
     // if event is TRIGGERED_BY_OBJECT_BIT
-    if( e->m.actbit&0x4000 )
+    if( e->acctype.actbit&0x4000 )
         // 0x00ff --> maximum event id number (act bit) is 255
-        event_mask_setbit(&(e->mask[1]),(e->m.actbit)&0x00ff);
+        event_mask_setbit(&(e->mask[1]),(e->acctype.actbit)&0x00ff);
     else
         // 0x00ff --> maximum event id number (act bit) is 255
-        event_mask_setbit(&(e->mask[0]),(e->m.actbit)&0x00ff);
+        event_mask_setbit(&(e->mask[0]),(e->acctype.actbit)&0x00ff);
 
     if( debug_def_out!=NULL )
     { char buf[16];
@@ -149,7 +149,7 @@ struct event_type_s *event_type_add( struct comm_s *comm, struct medusa_acctype_
         debug_def_out(debug_def_arg,comm->name);
         debug_def_out(debug_def_arg,"\"] event ");
         if( e->op[0]!=NULL )
-        {	debug_def_out(debug_def_arg,e->m.op_name[0]);
+        {	debug_def_out(debug_def_arg,e->acctype.op_name[0]);
             debug_def_out(debug_def_arg,":");
             debug_def_out(debug_def_arg,e->op[0]->m.name);
         }
@@ -158,22 +158,22 @@ struct event_type_s *event_type_add( struct comm_s *comm, struct medusa_acctype_
         debug_def_out(debug_def_arg,m->name);
         debug_def_out(debug_def_arg," ");
         if( e->op[1]!=NULL )
-        {	debug_def_out(debug_def_arg,e->m.op_name[1]);
+        {	debug_def_out(debug_def_arg,e->acctype.op_name[1]);
             debug_def_out(debug_def_arg,":");
             debug_def_out(debug_def_arg,e->op[1]->m.name);
         }
         else	debug_def_out(debug_def_arg,"-");
 
         debug_def_out(debug_def_arg," (");
-        sprintf(buf,"%d",(e->m.actbit)&0x00ff);
+        sprintf(buf,"%d",(e->acctype.actbit)&0x00ff);
         debug_def_out(debug_def_arg,buf);
         debug_def_out(debug_def_arg,")");
 
-        if( e->m.actbit&0x8000 )
+        if( e->acctype.actbit&0x8000 )
             debug_def_out(debug_def_arg," to object");
         else
             debug_def_out(debug_def_arg," to subject");
-        if( e->m.actbit&0x4000 )
+        if( e->acctype.actbit&0x4000 )
             debug_def_out(debug_def_arg," object's event");
         else
             debug_def_out(debug_def_arg," subject's event");
@@ -183,13 +183,13 @@ struct event_type_s *event_type_add( struct comm_s *comm, struct medusa_acctype_
         pthread_mutex_unlock(&debug_def_lock);
     }
 
-    if( (p=event_type_find_name(e->m.name))==NULL )
+    if( (evname=event_type_find_name(e->acctype.name))==NULL )
     {	free(e);
         return(NULL);
     }
-    p->events[comm->conn]=e;
-    e->evname=p;
-    hash_add(&(comm->events),&(e->hashent),e->m.opid);
+    evname->events[comm->conn]=e;
+    e->evname=evname;
+    hash_add(&(comm->events),&(e->hashent),e->acctype.opid);
     return(e);
 }
 

constable/event.h
@@ -34,11 +34,11 @@ struct event_names_s;
 struct event_type_s {
     struct hash_ent_s	hashent;
     struct event_names_s	*evname;
-    struct class_s		*op[2];
-    struct class_s		*object; /* komu sa nastavuje sledovanie */
+    struct class_s		*op[2];  /**< subject and object */
+    struct class_s		*monitored_operand; /**< points to op[0] or op[1] */
     event_mask_t		mask[2];
     //	struct event_type_s	*alt;
-    struct medusa_acctype_s	m;
+    struct medusa_acctype_s	acctype;
     struct class_s		operation_class;
 };
 

constable/generic.c
@@ -63,7 +63,7 @@ int generic_set_handler( struct class_handler_s *h, struct comm_s *comm, struct 
 #ifdef USE_ALT
     }
 #endif
-    if( inh && h->user!=NULL && ((struct event_names_s*)(h->user))->events[comm->conn]->object==o->class )
+    if( inh && h->user!=NULL && ((struct event_names_s*)(h->user))->events[comm->conn]->monitored_operand==o->class ) {
         object_add_event(o,((struct event_names_s*)(h->user))->events[comm->conn]->mask);
     //printf("ZZZ: ffffffffffffffff\n");
     return(0);
@@ -266,7 +266,7 @@ int generic_init_comm( struct class_handler_s *h, struct comm_s *comm )
     if( h->user!=NULL )
     {	if( (event=((struct event_names_s*)(h->user))->events[comm->conn])!=NULL )
         {	if( class!=event->op[0] )
-                comm->conf_error(comm,"generic_init_comm: class of tree %s is not a class of subject of operation %s",h->root->type->name,event->m.name);
+                comm->conf_error(comm,"generic_init_comm: class of tree %s is not a class of subject of operation %s",h->root->type->name,event->acctype.name);
         }
         else
         {	comm->conf_error(comm,"Undefined event %s",((struct event_names_s*)(h->user))->name);
@@ -274,7 +274,7 @@ int generic_init_comm( struct class_handler_s *h, struct comm_s *comm )
         }
     }
     // if event is TRIGGERED_AT_SUBJECT | TRIGGERED_BY_OBJECT_BIT
-    if( (event!=NULL && ((event->m.actbit&0xc000)==0x4000))
+    if( (event!=NULL && ((event->acctype.actbit&0xc000)==0x4000))
             || (x=class_alloc_subject_cinfo(class))<0 )
         x=class_alloc_object_cinfo(class);
     if( x<0 )

constable/mcp/mcp.c
@@ -62,9 +62,9 @@ static int get_event_context( struct comm_s *comm, struct event_context_s *c, st
 {
     c->operation.next=&(c->subject);
     c->operation.attr.offset=0;
-    c->operation.attr.length=t->m.size;
+    c->operation.attr.length=t->acctype.size;
     c->operation.attr.type=MED_TYPE_END;
-    strncpy(c->operation.attr.name,t->m.name,MIN(MEDUSA_ATTRNAME_MAX,MEDUSA_OPNAME_MAX));
+    strncpy(c->operation.attr.name,t->acctype.name,MIN(MEDUSA_ATTRNAME_MAX,MEDUSA_OPNAME_MAX));
     c->operation.flags=comm->flags;
     c->operation.class=&(t->operation_class);
     c->operation.data=(char*)(data)+2*sizeof(MCPptr_t);
@@ -72,17 +72,17 @@ static int get_event_context( struct comm_s *comm, struct event_context_s *c, st
     c->subject.next=&(c->object);
     c->subject.attr.offset=c->subject.attr.length=0;
     c->subject.attr.type=MED_TYPE_END;
-    strncpy(c->subject.attr.name,t->m.op_name[0],MEDUSA_ATTRNAME_MAX);
+    strncpy(c->subject.attr.name,t->acctype.op_name[0],MEDUSA_ATTRNAME_MAX);
     c->subject.flags=comm->flags;
     c->subject.class=t->op[0];
-    c->subject.data=(char*)(data)+2*sizeof(MCPptr_t)+(t->m.size);
+    c->subject.data=(char*)(data)+2*sizeof(MCPptr_t)+(t->acctype.size);
     c->object.next=NULL;
     c->object.attr.offset=c->object.attr.length=0;
     c->object.attr.type=MED_TYPE_END;
-    strncpy(c->object.attr.name,t->m.op_name[1],MEDUSA_ATTRNAME_MAX);
+    strncpy(c->object.attr.name,t->acctype.op_name[1],MEDUSA_ATTRNAME_MAX);
     c->object.flags=comm->flags;
     c->object.class=t->op[1];
-    c->object.data=(char*)(data)+2*sizeof(MCPptr_t)+(t->m.size);
+    c->object.data=(char*)(data)+2*sizeof(MCPptr_t)+(t->acctype.size);
     if( c->subject.class!=NULL )
     {	c->object.data+= c->subject.class->m.size;
         c->subject.attr.length=c->subject.class->m.size;
@@ -379,7 +379,7 @@ static read_result_e mcp_r_head( struct comm_buffer_s *b )
         {	comm_error("comm %s: Unknown access type %p!",b->comm->name,x);
             return READ_ERROR;
         }
-        b->want= sizeof(uint32_t) + b->len + (b->event->m.size);
+        b->want= sizeof(uint32_t) + b->len + (b->event->acctype.size);
         if( b->event->op[0]!=NULL )
             b->want += b->event->op[0]->m.size;
         if( b->event->op[1]!=NULL )

constable/rbac/adm.c
@@ -29,9 +29,9 @@ static int get_event_context( struct comm_s *comm, struct event_context_s *c, st
 {
     c->operation.next=&(c->subject);
     c->operation.attr.offset=0;
-    c->operation.attr.length=t->m.size;
+    c->operation.attr.length=t->acctype.size;
     c->operation.attr.type=MED_TYPE_END;
-    strncpy(c->operation.attr.name,t->m.name,MIN(MEDUSA_ATTRNAME_MAX,MEDUSA_OPNAME_MAX));
+    strncpy(c->operation.attr.name,t->acctype.name,MIN(MEDUSA_ATTRNAME_MAX,MEDUSA_OPNAME_MAX));
     c->operation.flags=comm->flags;
     c->operation.class=&(t->operation_class);
     c->operation.data=(char*)(op);
@@ -39,14 +39,14 @@ static int get_event_context( struct comm_s *comm, struct event_context_s *c, st
     c->subject.next=&(c->object);
     c->subject.attr.offset=c->subject.attr.length=0;
     c->subject.attr.type=MED_TYPE_END;
-    strncpy(c->subject.attr.name,t->m.op_name[0],MEDUSA_ATTRNAME_MAX);
+    strncpy(c->subject.attr.name,t->acctype.op_name[0],MEDUSA_ATTRNAME_MAX);
     c->subject.flags=subject->flags;
     c->subject.class=subject->class;
     c->subject.data=subject->data;
     c->object.next=NULL;
     c->object.attr.offset=c->object.attr.length=0;
     c->object.attr.type=MED_TYPE_END;
-    strncpy(c->object.attr.name,t->m.op_name[1],MEDUSA_ATTRNAME_MAX);
+    strncpy(c->object.attr.name,t->acctype.op_name[1],MEDUSA_ATTRNAME_MAX);
     c->object.flags=comm->flags;
     c->object.class=t->op[1];
     c->object.data=(char*)(object);

constable/rbac/proc.c
@@ -141,7 +141,7 @@ static int rbac_proc_init_comm( struct class_handler_s *h, struct comm_s *comm )
             return(-1);
         }
     }
-    if( (event!=NULL && ((event->m.actbit&0xc000)==0x4000))
+    if( (event!=NULL && ((event->acctype.actbit&0xc000)==0x4000))
             || (x=class_alloc_subject_cinfo(class))<0 )
         x=class_alloc_object_cinfo(class);
     if( x<0 )

constable/space.c
@@ -267,7 +267,7 @@ struct tree_add_event_mask_do_s {
 };
 static void tree_add_event_mask_do( struct tree_s *p, struct tree_add_event_mask_do_s *arg )
 {
-    if( arg->type->object==p->type->class_handler->classname->classes[arg->conn] )
+    if( arg->type->monitored_operand==p->type->class_handler->classname->classes[arg->conn] )
         event_mask_or2(p->events[arg->conn].event,arg->type->mask);
 }
 
@@ -378,8 +378,8 @@ static void tree_comm_reinit( struct comm_s *comm, struct tree_s *t )
                 {	comm->conf_error(comm,"Unknown event %s\n",hh->handler->op_name);
                     continue;
                 }
-                if( type->object==type->op[0] )
-                {	if( type->object==t->type->class_handler->classname->classes[comm->conn] )
+                if( type->monitored_operand==type->op[0] )
+                {	if( type->monitored_operand==t->type->class_handler->classname->classes[comm->conn] )
                         event_mask_or2(t->events[comm->conn].event,type->mask);
                     else	comm->conf_error(comm,"event %s subject class mismatch",hh->handler->op_name);
                 }
@@ -390,8 +390,8 @@ static void tree_comm_reinit( struct comm_s *comm, struct tree_s *t )
                 {	comm->conf_error(comm,"Unknown event %s\n",hh->handler->op_name);
                     continue;
                 }
-                if( type->object==type->op[1] )
-                {	if( type->object==t->type->class_handler->classname->classes[comm->conn] )
+                if( type->monitored_operand==type->op[1] )
+                {	if( type->monitored_operand==t->type->class_handler->classname->classes[comm->conn] )
                         event_mask_or2(t->events[comm->conn].event,type->mask);
                     else	comm->conf_error(comm,"event %s object class mismatch",hh->handler->op_name);
                 }
@@ -423,13 +423,13 @@ int space_init_event_mask( struct comm_s *comm )
                     ((type->op[1]==NULL) != (le->object==NULL)) )
             {	comm->conf_error(comm,"Invalid use of event %s\n",le->handler->op_name);
             }
-            if( le->subject!=NULL && le->subject!=ALL_OBJ && type->object==type->op[0] )
+            if( le->subject!=NULL && le->subject!=ALL_OBJ && type->monitored_operand==type->op[0] )
             { struct tree_add_event_mask_do_s arg;
                 arg.conn=comm->conn;
                 arg.type=type;
                 space_for_each_path(le->subject,(fepf_t)tree_add_event_mask_do,&arg);
             }
-            if( le->object!=NULL && le->object!=ALL_OBJ && type->object==type->op[1] )
+            if( le->object!=NULL && le->object!=ALL_OBJ && type->monitored_operand==type->op[1] )
             { struct tree_add_event_mask_do_s arg;
                 arg.conn=comm->conn;
                 arg.type=type;

commit 52a0fef5f4ff271eecaa71bc58ec6094736e7fa6

fix: debug output (regression from a06e00b)

constable/event.c
@@ -357,9 +357,9 @@ static int do_event_list( struct comm_buffer_s *cb )
 
 #if 0
     //printf("ZZZ cb->ehh_list=%d\n",cb->ehh_list);
-    if( strcmp(cb->event->m.name,"getfile") /* nie iget na subory */ )
+    if( strcmp(cb->event->acctype.name,"getfile") /* nie iget na subory */ )
     {
-        printf("ZZZ: event: op=%s (%d) ",cb->event->m.name,cb->do_phase);
+        printf("ZZZ: event: op=%s (%d) ",cb->event->acctype.name,cb->do_phase);
         if( c->subject.class!=NULL )
             printf("subject=%s ",c->subject.class->m.name);
         else
@@ -473,7 +473,7 @@ static int do_event_list( struct comm_buffer_s *cb )
      * handler, class handler for object or subject). */
     if( c->unhandled )
     {
-        //printf("do_event_list: No event executed! [%s]\n",cb->event->evname->name);
+        //printf("do_event_list: No event executed! [%s]\n",cb->event->acctype->name);
         return(-1);
     }
     //printf("do_event_list: return 0, event result is %d\n",c->result);