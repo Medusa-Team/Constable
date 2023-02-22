Medusa-Team/Constable

commit 6dfc95e7236cc2da5c25ee90ebc06b12ca8093a6

refactor: simplify VS allocation

constable/space.c
@@ -461,13 +461,13 @@ vs_t *space_get_vs( struct space_s *space )
 {
     if( space==NULL || space==ALL_OBJ)
         return((vs_t*)space);
-    if( space->my_vs!=NULL )
-        return(space->my_vs->vs);
-    if( (space->my_vs=vs_alloc(space->name))==NULL )
+    if( !vs_isclear(space->vs_id) )
+        return(space->vs_id);
+    if( vs_alloc(space->vs_id)<0 )
         return(NULL);
-    //printf("ZZZ:space_get_vs %s 0x%08x\n",space->name,*(space->my_vs->vs));
-    vs_add(space->my_vs->vs,space->vs[AT_MEMBER]);
-    return(space->my_vs->vs);
+    //printf("ZZZ:space_get_vs %s 0x%08x\n",space->name,*(space->vs_id));
+    vs_add(space->vs_id,space->vs[AT_MEMBER]);
+    return(space->vs_id);
 }
 
 /* ----------------------------------- */

constable/language/error.c
@@ -20,6 +20,7 @@ extern compiler_class_t *global_compiler;
 pthread_key_t errstr_key;
 char *Out_of_memory="Out of memory";
 char *Space_already_defined="Space already defined";
+char *Out_of_vs="Out of available VS";
 
 int error( const char *fmt, ... )
 { va_list ap;

constable/language/error.h
@@ -14,6 +14,7 @@
 extern pthread_key_t errstr_key;
 extern char *Out_of_memory;
 extern char *Space_already_defined;
+extern char *Out_of_vs;
 
 int error( const char *fmt, ... );
 int warning( const char *fmt, ... );

constable/vs.c
@@ -6,12 +6,11 @@
 
 #include "constable.h"
 #include "vs.h"
-#include "space.h" // only for ANON_SPACE_NAME
 #include <pthread.h>
 
 //static int number_of_vs=MAX_NUM_OF_VS/32;
 static struct vs_s vs_tab;
-static int vs;
+static int next_vs_id;
 
 //int vs_init( int max_vs )
 int vs_init( void )
@@ -34,36 +33,23 @@ int vs_init( void )
     }
     number_of_vs= max_vs/32;
 */
-    vs=0;
+    next_vs_id=0;
     return(0);
 }
 
 /* ---------- vs alloc/free ---------- */
-struct vs_s *vs_alloc( char *name )
-{ struct vs_s *p;
-    if( vs >= MAX_NUM_OF_VS )
+int vs_alloc( vs_t *id )
+{
+    if( next_vs_id >= MAX_NUM_OF_VS )
     {
         char **errstr = (char**) pthread_getspecific(errstr_key);
-        *errstr=Out_of_memory;
-        return(NULL);
+        *errstr=Out_of_vs;
+        return(-1);
     }
-    for(p=&vs_tab;p->next!=NULL;p=p->next)
-    {	if( name[0]!=ANON_SPACE_NAME[0] && !strcmp(name,p->next->name) )
-        {	char **errstr = (char**) pthread_getspecific(errstr_key);
-            *errstr=Space_already_defined;
-            return(NULL);
-        }
-    }
-    if( (p->next=calloc(1,sizeof(struct vs_s)+strlen(name)+1))==NULL )
-    {	char **errstr = (char**) pthread_getspecific(errstr_key);
-        *errstr=Out_of_memory;
-        return(NULL);
-    }
-    p->next->next=NULL;
-    strcpy(p->next->name,name);
-    p->next->vs[vs/BITS_PER_VS_WORD] |= 1 << (vs % BITS_PER_VS_WORD);
-    vs += 1;
-    return(p->next);
+    vs_clear(id);
+    id[next_vs_id/BITS_PER_VS_WORD] |= 1 << (next_vs_id % BITS_PER_VS_WORD);
+    next_vs_id += 1;
+    return(0);
 }
 
 int vs_is_enough( int bites )

constable/vs.h
@@ -22,7 +22,7 @@ struct vs_s {
 };
 
 int vs_init( void );
-struct vs_s *vs_alloc( char *name );
+int vs_alloc( vs_t *id );
 int vs_is_enough( int bites );
 
 void vs_clear( vs_t *to );

commit cc300a7a3384f2272d6ccff431833c94390dfb75

fix: clear vs_id when allocating new space_s
Fixes: 6dfc95e ("refactor: simplify VS allocation")
Co-authored-by: Matus Jokay <matus.jokay@stuba.sk>

constable/space.c
@@ -99,6 +99,7 @@ struct space_s *space_create(char *name, int primary)
 	strcpy(t->name, name);
 	for (a = 0; a < NR_ACCESS_TYPES; a++)
 		vs_clear(t->vs[a]);
+        vs_clear(t->vs_id);
 	t->levent = NULL;
 	t->ltree = NULL;
 	t->primary = primary;