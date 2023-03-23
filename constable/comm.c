Medusa-Team/Constable

commit 71ddf8931d9d922ee8760945bc5272ccebc7d96a

feat: add READY cmd to the communication protocol
1) move comm_conn_init() from mcp_r_query() to mcp_r_ready_request():
   comm_conn_init() is now not running with first decide request, but
   with MEDUSA_COMM_READY_REQUEST
2) move _init() running, if defined in medusa's configuration file, from
   mcp_r_query() (running with/before first decide request) into
   comm_conn_init()

constable/comm.c
@@ -26,6 +26,7 @@ int comm_buf_to_queue_locked(struct comm_buffer_queue_s *q,
 struct comm_buffer_s *comm_buf_from_queue_locked(struct comm_buffer_queue_s *q);
 static pthread_t comm_workers[N_WORKER_THREADS];
 
+extern struct event_handler_s *function_init;
 int comm_nr_connections;
 static struct comm_s *first_comm;
 static struct comm_s *last_comm;
@@ -121,6 +122,7 @@ int comm_do(void)
 	for (c = first_comm; c; c = c->next) {
 		if (c->fd < 0)
 			continue;
+
 		if (mcp_receive_greeting(c)) {
 			comm_error("Error when receiving greeting");
 			c->close(c);
@@ -315,6 +317,34 @@ int comm_conn_init(struct comm_s *comm)
 		return -1;
 	if (class_comm_init(comm) < 0)
 		return -1;
+
+	/*
+	 * If the configuration file defines _init(), it is inserted into the
+	 * queue first and decision requests that came from the kernel are
+	 * inserted into `init_buffer->to_wake` queue to be processed after
+	 * _init() finishes.
+	 */
+	pthread_mutex_lock(&comm->state_lock);
+	if (function_init) {
+		struct comm_buffer_s *p;
+
+		p = comm_buf_get(0, comm);
+		if (unlikely(!p)) {
+			comm_error("Can't get comm buffer for _init");
+			pthread_mutex_unlock(&comm->state_lock);
+			return -1;
+		}
+		get_empty_context(&p->context);
+		p->event = NULL;
+		p->init_handler = function_init;
+		p->ehh_list = EHH_NOTIFY_ALLOW;
+		comm->init_buffer = p;
+		// _init() is inserted here
+		comm_buf_todo(p);
+	}
+	comm->state = 1;
+	pthread_mutex_unlock(&comm->state_lock);
+
 	return 0;
 }
 

constable/comm_buf.c
@@ -120,6 +120,7 @@ struct comm_buffer_s *comm_buf_resize(struct comm_buffer_s *b, int size)
 	return b;
 }
 
+int mcp_ready_answer(struct comm_s *c);
 
 static void comm_buf_free(struct comm_buffer_s *b)
 {
@@ -129,6 +130,10 @@ static void comm_buf_free(struct comm_buffer_s *b)
 	while ((q = comm_buf_from_queue(&(b->to_wake))) != NULL)
 		comm_buf_todo(q);
 	pthread_mutex_unlock(&(b->to_wake.lock));
+
+	/* send READY cmd to the kernel after _init() finishes */
+	if (function_init && b->comm->init_buffer == b && mcp_ready_answer(b->comm) < 0)
+		fatal("%s: MEDUSA_COMM_READY_ANSWER not send to the kernel", __func__);
 
 	//printf("comm_buf_free: free buffer %u\n", b->id);
 	if (b->_n >= 0) {

constable/mcp/mcp.c
@@ -50,6 +50,7 @@ static int mcp_answer(struct comm_s *c, struct comm_buffer_s *b);
 static enum read_result mcp_r_classdef_attr(struct comm_buffer_s *b);
 static enum read_result mcp_r_acctypedef_attr(struct comm_buffer_s *b);
 static enum read_result mcp_r_discard(struct comm_buffer_s *b);
+static enum read_result mcp_r_ready_request(struct comm_buffer_s *b);
 static int mcp_write(struct comm_s *c);
 static int mcp_nowrite(struct comm_s *c);
 static int mcp_close(struct comm_s *c);
@@ -457,6 +458,10 @@ static enum read_result mcp_r_head(struct comm_buffer_s *b)
 		b->want = b->len + 2 * sizeof(MCPptr_t) + sizeof(unsigned int);
 		b->completed = mcp_r_update_answer;
 		break;
+	case MEDUSA_COMM_READY_REQUEST:
+		/* only redirect to mcp_r_ready_request() */
+		b->completed = mcp_r_ready_request;
+		break;
 	default:
 		comm_error("comm %s: Communication protocol error! (%d)",
 			   b->comm->name, ((unsigned int *)(b->comm_buf + sizeof(MCPptr_t)))[0]);
@@ -477,42 +482,13 @@ static enum read_result mcp_r_query(struct comm_buffer_s *b)
 	pthread_mutex_lock(&b->comm->state_lock);
 
 	/*
-	 * If this is the first decision request from the kernel.
+	 * ParanoYa sanity check: `comm->init_buffer` is not allocated yet.
 	 */
 	if (unlikely(b->comm->state == 0)) {
-		if (unlikely(comm_conn_init(b->comm) < 0)) {
-			pthread_mutex_unlock(&b->comm->state_lock);
-			return READ_ERROR;
-		}
-		/*
-		 * If the configuration file defines _init(), it is inserted into the
-		 * queue first and decision request that came from the kernel is
-		 * inserted into `init_buffer->to_wake` queue to be processed after
-		 * _init() finishes.
-		 */
-		if (function_init) {
-			struct comm_buffer_s *p;
-
-			p = comm_buf_get(0, b->comm);
-			if (unlikely(!p)) {
-				comm_error("Can't get comm buffer for _init");
-				pthread_mutex_unlock(&b->comm->state_lock);
-				return READ_ERROR;
-			}
-			get_empty_context(&p->context);
-			p->event = NULL;
-			p->init_handler = function_init;
-			comm_buf_to_queue(&p->to_wake, b);
-			p->ehh_list = EHH_NOTIFY_ALLOW;
-			b->comm->state = 1;
-			b->comm->init_buffer = p;
-			// _init() is inserted here
-			comm_buf_todo(p);
-			pthread_mutex_unlock(&b->comm->state_lock);
-			b->completed = NULL;
-			return READ_DONE;
-		}
-		b->comm->state = 1;
+		comm_error("Corrupted comm '%s' state: comm->init_buffer is not allocated yet!",
+			   b->comm->name);
+		pthread_mutex_unlock(&b->comm->state_lock);
+		return READ_ERROR;
 	}
 
 	if (function_init && b->comm->init_buffer) {
@@ -707,13 +683,55 @@ static enum read_result mcp_r_discard(struct comm_buffer_s *b)
 	return READ_FREE;
 }
 
+int mcp_ready_answer(struct comm_s *c)
+{
+	struct comm_buffer_s *r;
+	MCPptr_t *out;
+
+	r = comm_buf_get(sizeof(MCPptr_t), c);
+	if (unlikely(!r)) {
+		fatal("Can't alloc buffer for COMM_READY answer!");
+		return -1;
+	}
+	out = (void *)&r->comm_buf;
+	*out = byte_reorder_put_int64(c->flags, MEDUSA_COMM_READY_ANSWER);
+	r->len = sizeof(*out);
+	r->want = 0;
+	r->completed = NULL;
+
+	comm_buf_output_enqueue(c, r);
+	return 0;
+}
+
+static enum read_result mcp_r_ready_request(struct comm_buffer_s *b)
+{
+	b->completed = NULL;
+
+	/* initialize comm and allocate a buffer for _init(), if defined in med config file */
+	if (comm_conn_init(b->comm) < 0)
+		return READ_ERROR;
+
+	/*
+	 * If _init() is not defined, initialization is complete, send READY_ANSWER to kernel.
+	 * If it's defined, READY_ANSWER is send after _init() finishes, from comm_buf_free()
+	 * when the _init()'s buffer will be destroyed
+	 */
+	if (!function_init && mcp_ready_answer(b->comm) < 0) {
+		fatal("%s: MEDUSA_COMM_READY_ANSWER not send to the kernel", __func__);
+		return READ_ERROR;
+	}
+
+	return READ_FREE;
+}
+
 /*
  * Each comm has its own thread performing `mcp_write` function.
  * A buffer is inserted into output queue of the comm interface `c`
- * from three operations:
+ * from four operations:
  * 1) answer (at the end of the processing of the kernel request)
  * 2) update (when updating an object on the kernel site)
  * 3) fetch (when requesting an object from the kernel site)
+ * 4) ready answer (an answer to ready query from the kernel site after initialization)
  *
  * Using of this function requires that the buffers in the output queue are
  * not used from another threads of Constable. Why? This function always

constable/medusa_object.h
@@ -39,6 +39,9 @@ typedef uint64_t Mptr_t; // medusa pointer if you want to run effectivly medusa 
 #define MEDUSA_COMM_ACCTYPEDEF		0x04	/* k->c */
 #define MEDUSA_COMM_ACCTYPEUNDEF	0x05	/* k->c */
 
+#define MEDUSA_COMM_READY_REQUEST	0x06	/* k->c */
+#define MEDUSA_COMM_READY_ANSWER	0x86	/* c->k */
+
 #define MEDUSA_COMM_FETCH_REQUEST	0x88	/* c->k */
 #define MEDUSA_COMM_FETCH_ANSWER	0x08	/* k->c */
 #define MEDUSA_COMM_FETCH_ERROR		0x09	/* k->c */

constable/minimal/y.conf
@@ -78,7 +78,8 @@ function log_proc {
 	return ALLOW;
 }
 
-//function _init
-//{
-//}
+function _init
+{
+	log("hehe");
+}
 

commit 0b7eddee19755114207f3839902f8e163ed85f7b

fix: comm protocol v2 regression introduced by 71ddf89
Commit 71ddf89 (feat: add READY cmd to the communication protocol,
2023-03-21) introduced support for new READY cmd in Medusa's comm
protocol, but caused a regression with older Medusa's protocol v2.

This commit fixes the regression and supports all comm protocols
from v2.

constable/comm.c
@@ -307,7 +307,7 @@ void *write_loop(void *arg)
 	return (void *)-1;
 }
 
-int comm_conn_init(struct comm_s *comm)
+int comm_conn_init(struct comm_s *comm, bool use_lock)
 {
 	//printf("ZZZ: comm_conn_init %s\n",comm->name);
 	/* default kobjects fo internal constable use */
@@ -324,7 +324,8 @@ int comm_conn_init(struct comm_s *comm)
 	 * inserted into `init_buffer->to_wake` queue to be processed after
 	 * _init() finishes.
 	 */
-	pthread_mutex_lock(&comm->state_lock);
+	if (use_lock)
+		pthread_mutex_lock(&comm->state_lock);
 	if (function_init) {
 		struct comm_buffer_s *p;
 
@@ -343,7 +344,8 @@ int comm_conn_init(struct comm_s *comm)
 		comm_buf_todo(p);
 	}
 	comm->state = 1;
-	pthread_mutex_unlock(&comm->state_lock);
+	if (use_lock)
+		pthread_mutex_unlock(&comm->state_lock);
 
 	return 0;
 }

constable/comm.h
@@ -156,7 +156,7 @@ int comm_alloc_buf_var_data(int size);
 struct comm_s *comm_new(char *name, int user_size);
 struct comm_s *comm_find(char *name);
 
-int comm_conn_init(struct comm_s *comm);
+int comm_conn_init(struct comm_s *comm, bool use_lock);
 
 int buffers_init(void);
 int buffers_alloc(void);

constable/comm_buf.c
@@ -132,7 +132,8 @@ static void comm_buf_free(struct comm_buffer_s *b)
 	pthread_mutex_unlock(&(b->to_wake.lock));
 
 	/* send READY cmd to the kernel after _init() finishes */
-	if (function_init && c->comm && b->comm->init_buffer == b && mcp_ready_answer(b->comm) < 0)
+	if (function_init && b->comm && b->comm->version > 2 && b->comm->init_buffer == b
+	    && mcp_ready_answer(b->comm) < 0)
 		fatal("%s: MEDUSA_COMM_READY_ANSWER not send to the kernel", __func__);
 
 	//printf("comm_buf_free: free buffer %u\n", b->id);

constable/mcp/mcp.c
@@ -484,14 +484,31 @@ static enum read_result mcp_r_query(struct comm_buffer_s *b)
 
 	/*
 	 * ParanoYa sanity check: `comm->init_buffer` is not allocated yet.
+	 *
+	 * For protocol version >= 3 this is a bug, for version < 3 this is a legitimate
+	 * state: this is the first decision request from the kernel and the comm interface
+	 * should be initialized.
 	 */
 	if (unlikely(b->comm->state == 0)) {
+		if (b->comm->version < 3) {
+			/*
+			 * Initialize comm and allocate a buffer for _init(), if defined in med
+			 * config file. Do not use locking; b->comm->state_lock is already held.
+			 */
+			if (unlikely(comm_conn_init(b->comm, false) < 0)) {
+				pthread_mutex_unlock(&b->comm->state_lock);
+				return READ_ERROR;
+			}
+			goto after_init;
+		}
+
 		comm_error("Corrupted comm '%s' state: comm->init_buffer is not allocated yet!",
 			   b->comm->name);
 		pthread_mutex_unlock(&b->comm->state_lock);
 		return READ_ERROR;
 	}
 
+after_init:
 	if (function_init && b->comm->init_buffer) {
 		/* Enqueue incoming requests to be processed after _init() finishes. */
 		comm_buf_to_queue(&b->comm->init_buffer->to_wake, b);
@@ -709,7 +726,7 @@ static enum read_result mcp_r_ready_request(struct comm_buffer_s *b)
 	b->completed = NULL;
 
 	/* initialize comm and allocate a buffer for _init(), if defined in med config file */
-	if (comm_conn_init(b->comm) < 0)
+	if (comm_conn_init(b->comm, true) < 0)
 		return READ_ERROR;
 
 	/*

constable/rbac/kobjects.c
@@ -303,7 +303,7 @@ static struct medusa_comm_class_s rbac_user_mclass={
                         return(init_error("Can't add classhandler for ROLE/"));
                     }
 
-                    comm_conn_init(rbac_comm);
+                    comm_conn_init(rbac_comm, true);
                     return(0);
                 }
 